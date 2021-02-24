a0c5bd00924ecdb5be0285408c65f40abf75d1e6
przemek83 <4788832+przemek83@users.noreply.github.com>
2021-02-24 10:54:30 +0100

    Replaced water
    Replaced water bitmap.

--- map/water.cpp
+++ map/water.cpp
@@ -1,18 +1,20 @@
 #include "water.h"
 #include "../config.h"
 
-Water::Water(){
-	this->id = 2;
-	this->fly = true;
-	this->drive = false;
-	this->armor = 0;
-	this->source = "image/plansza/plansza_woda.tga";
-	if(!loadBitmap()){
-		cout << "Bricks Error" << "\n";
-		exit(0);
-	this->armorUp=0;
-	this->levelUp=0;
-	this->speedUp=0;
-	this->tankUp=0;
-	}
+Water::Water()
+{
+    this->id = 2;
+    this->fly = true;
+    this->drive = false;
+    this->armor = 0;
+    this->source = "image/board/water.tga";
+    if (!loadBitmap())
+    {
+        cout << "Water Error" << std::endl;
+        exit(0);
+        this->armorUp = 0;
+        this->levelUp = 0;
+        this->speedUp = 0;
+        this->tankUp = 0;
+    }
 }
