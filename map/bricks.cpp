98558e9114ccf977d7bf7beb4c2f549fef2a686a
przemek83 <4788832+przemek83@users.noreply.github.com>
2021-02-24 09:13:52 +0100

    New bricks bitmap
    Use new bricks as map element

--- map/bricks.cpp
+++ map/bricks.cpp
@@ -1,17 +1,19 @@
 #include "bricks.h"
 #include <iostream>
 
-Bricks::Bricks(){
-	this->id = 1;
-	this->fly = false;
-	this->drive = false;
-	this->armor = 2;
-	this->source = "image/plansza/plansza_cegla.tga";
-	if(!loadBitmap()){
-		exit(0);
-	this->armorUp=0;
-	this->levelUp=0;
-	this->speedUp=0;
-	this->tankUp=0;
-	}
+Bricks::Bricks()
+{
+    this->id = 1;
+    this->fly = false;
+    this->drive = false;
+    this->armor = 2;
+    this->source = "image/board/brick.tga";
+    if (!loadBitmap())
+    {
+        exit(0);
+        this->armorUp = 0;
+        this->levelUp = 0;
+        this->speedUp = 0;
+        this->tankUp = 0;
+    }
 }
