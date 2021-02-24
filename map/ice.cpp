f8a3e46e51e0b29b2819ef34064ed1b568412740
przemek83 <4788832+przemek83@users.noreply.github.com>
2021-02-24 10:59:42 +0100

    Replaced ice
    Replaced ice bitmap by new one.

--- map/ice.cpp
+++ map/ice.cpp
@@ -1,17 +1,19 @@
 #include "ice.h"
 #include <iostream>
 
-Ice::Ice(){
-	this->id = 4;
-	this->fly = true;
-	this->drive = true;
-	this->armor = 999;
-	this->source = "image/plansza/plansza_lod.tga";
-	if(!loadBitmap()){
-		exit(0);
-	}
-	this->armorUp=0;
-	this->levelUp=0;
-	this->speedUp=0;
-	this->tankUp=0;
+Ice::Ice()
+{
+    this->id = 4;
+    this->fly = true;
+    this->drive = true;
+    this->armor = 999;
+    this->source = "image/board/ice.tga";
+    if (!loadBitmap())
+    {
+        exit(0);
+    }
+    this->armorUp = 0;
+    this->levelUp = 0;
+    this->speedUp = 0;
+    this->tankUp = 0;
 }
