5da88b1885f95a1a552e90f44cb5067b65d95e30
Przemek <4788832+przemek83@users.noreply.github.com>
2023-01-28 16:54:57 +0100

    Update BulletTest.cpp
    Some test cases for Bullet class.

--- test/BulletTest.cpp
+++ test/BulletTest.cpp
@@ -1,6 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include "Bullet.h"
+#include "Config.h"
 #include "Direction.h"
 #include "TankType.h"
 
@@ -14,8 +15,52 @@ static Bullet getExampleBullet()
             Direction::UP};
 }
 
-TEST_CASE("Bullet power")
+TEST_CASE("Bullet getters", "[bullet]")
 {
     const Bullet bullet{getExampleBullet()};
-    REQUIRE(bullet.getPower() == bulletPower);
+
+    SECTION("bullet power is set")
+    {
+        REQUIRE(bullet.getPower() == bulletPower);
+    }
+    SECTION("tank type is set")
+    {
+        REQUIRE(bullet.getTankType() == TankType::ENEMY_TIER_1);
+    }
+    SECTION("resource type is set")
+    {
+        REQUIRE(bullet.getResourceType() == ResourceType::BULLET);
+    }
+}
+
+TEST_CASE("Bullet coordinates", "[bullet]")
+{
+    const Bullet bullet{getExampleBullet()};
+
+    SECTION("location is correct")
+    {
+        const Point currentPoint{bullet.getLocation()};
+        REQUIRE(currentPoint.x == point.x - Config::BULLET_SIZE / 2);
+        REQUIRE(currentPoint.y == point.y - Config::BULLET_SIZE / 2);
+    }
+    SECTION("center is correct")
+    {
+        const Point currentPoint{bullet.getCenter()};
+        REQUIRE(currentPoint.x == point.x);
+        REQUIRE(currentPoint.y == point.y);
+    }
+
+    SECTION("getX is working")
+    {
+        const Point currentPoint{bullet.getLocation()};
+        const unsigned int currentX{bullet.getX()};
+        REQUIRE(currentX == currentPoint.x);
+    }
+
+    SECTION("getY is working")
+    {
+        const Point currentPoint{bullet.getLocation()};
+        const unsigned int currentY{bullet.getY()};
+        REQUIRE(currentY == currentPoint.y);
+    }
 }
