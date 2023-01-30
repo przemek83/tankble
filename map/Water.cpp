b0a1e5d3b327460f1d453b0c4316f5bc769b85cc
Przemek <4788832+przemek83@users.noreply.github.com>
2023-01-30 15:41:05 +0100

    Update Water.cpp
    Make water not crossable.

--- map/Water.cpp
+++ map/Water.cpp
@@ -4,6 +4,6 @@ Water::Water(Point point) : Tile(NO_ARMOR, point) {}
 
 bool Water::canFly() { return true; }
 
-bool Water::canDrive() { return true; }
+bool Water::canDrive() { return false; }
 
 ResourceType Water::getResourceType() const { return ResourceType::WATER; }
