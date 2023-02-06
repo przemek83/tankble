2dd2f750046f9800bed7f7bc29e191a5d260b03e
Przemek <4788832+przemek83@users.noreply.github.com>
2023-02-06 19:54:25 +0100

    Update TankTest.cpp
    Added direction related tests.

--- test/TankTest.cpp
+++ test/TankTest.cpp
@@ -29,3 +29,27 @@ TEST_CASE("Tank creation", "[tank]")
         REQUIRE(tank.getCenter() == Point{25, 25});
     }
 }
+
+TEST_CASE("Tank direction", "[tank]")
+{
+    const Point point{10, 10};
+    SECTION("check direction - enemy")
+    {
+        const Tank tank(TankType::ENEMY_TIER_1, point);
+        REQUIRE(tank.getDirection() == Direction::DOWN);
+    }
+
+    SECTION("check direction - player")
+    {
+        const Tank tank(TankType::PLAYER_TIER_1, point);
+        REQUIRE(tank.getDirection() == Direction::UP);
+    }
+
+    SECTION("set direction")
+    {
+        Tank tank(TankType::PLAYER_TIER_1, point);
+        const Direction newDirection{Direction::LEFT};
+        tank.setDirection(newDirection);
+        REQUIRE(tank.getDirection() == newDirection);
+    }
+}
