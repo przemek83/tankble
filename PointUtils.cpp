fa01e6a3104564e4a60f997eaae2af1e851ed3a5
Przemek <4788832+przemek83@users.noreply.github.com>
2023-04-16 12:41:14 +0200

    Update PointUtils.cpp
    Removed not used functions.

--- PointUtils.cpp
+++ PointUtils.cpp
@@ -9,19 +9,10 @@ Point pointFromSigned(int x, int y)
     return {static_cast<unsigned int>(x), static_cast<unsigned int>(y)};
 }
 
-bool isValidPoint(int x, int y) { return isValidPoint(x, y, 1); }
-
 bool isValidPoint(int x, int y, unsigned int objectSize)
 {
     const int maxCoordinate{static_cast<int>(
         Config::getInstance().getBoardWidth() - (objectSize - 1))};
     return x < maxCoordinate && y < maxCoordinate && y >= 0 && x >= 0;
 }
-
-bool isValidPoint(Point point)
-{
-    const unsigned int maxCoordinate{Config::getInstance().getBoardWidth()};
-    return point.x < maxCoordinate && point.y < maxCoordinate;
-}
-
 };  // namespace PointUtils
