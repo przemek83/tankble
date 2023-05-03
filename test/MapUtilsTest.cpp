bb9b9fda4724a8ae0bf7878b56e35c65419218d0
Przemek <4788832+przemek83@users.noreply.github.com>
2023-05-03 16:11:58 +0200

    Update MapUtilsTest.cpp
    Additional tests for MapUtils::getMovePoints().

--- test/MapUtilsTest.cpp
+++ test/MapUtilsTest.cpp
@@ -12,4 +12,36 @@ TEST_CASE("Move points", "[MapUtils]")
             MapUtils::getMovePoints(leftUpperCorner, Direction::UP)};
         REQUIRE(movePoints.size() == 2);
     }
+
+    SECTION("moving up")
+    {
+        const std::vector<Point> movePoints{
+            MapUtils::getMovePoints(leftUpperCorner, Direction::UP)};
+        REQUIRE(movePoints.front() == Point{110, 100});
+        REQUIRE(movePoints.back() == Point{120, 100});
+    }
+
+    SECTION("moving down")
+    {
+        const std::vector<Point> movePoints{
+            MapUtils::getMovePoints(leftUpperCorner, Direction::DOWN)};
+        REQUIRE(movePoints.front() == Point{110, 129});
+        REQUIRE(movePoints.back() == Point{120, 129});
+    }
+
+    SECTION("moving right")
+    {
+        const std::vector<Point> movePoints{
+            MapUtils::getMovePoints(leftUpperCorner, Direction::RIGHT)};
+        REQUIRE(movePoints.front() == Point{129, 110});
+        REQUIRE(movePoints.back() == Point{129, 120});
+    }
+
+    SECTION("moving left")
+    {
+        const std::vector<Point> movePoints{
+            MapUtils::getMovePoints(leftUpperCorner, Direction::LEFT)};
+        REQUIRE(movePoints.front() == Point{100, 110});
+        REQUIRE(movePoints.back() == Point{100, 120});
+    }
 }
