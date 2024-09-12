224c8b52e132f558dd8ec1a8d3cf13767a5b7621
przemek83 <4788832+przemek83@users.noreply.github.com>
2024-09-12 11:54:58 +0200

    Rename fields in TankStat to match convention.

--- src/Status.cpp
+++ src/Status.cpp
@@ -7,9 +7,9 @@ Status::Status(Point point) : Drawable(point) {}
 
 void Status::update(TankStats newStats, const Screen& screen)
 {
-    if (stats_.attackPower != newStats.attackPower ||
-        stats_.shield != newStats.shield || stats_.lives != newStats.lives ||
-        stats_.speed != newStats.speed)
+    if (stats_.attackPower_ != newStats.attackPower_ ||
+        stats_.shield_ != newStats.shield_ ||
+        stats_.lives_ != newStats.lives_ || stats_.speed_ != newStats.speed_)
     {
         stats_ = newStats;
         draw(screen);
@@ -20,14 +20,14 @@ void Status::draw(const Screen& screen) const
 {
     const unsigned int spacer{getHeight() / 5};
     screen.drawTextWithBackground(getCenter().x_, spacer * 1,
-                                  "Lives: " + std::to_string(stats_.lives));
+                                  "Lives: " + std::to_string(stats_.lives_));
     screen.drawTextWithBackground(getCenter().x_, spacer * 2,
-                                  "Shield: " + std::to_string(stats_.shield));
+                                  "Shield: " + std::to_string(stats_.shield_));
     screen.drawTextWithBackground(getCenter().x_, spacer * 3,
-                                  "Speed: " + std::to_string(stats_.speed));
+                                  "Speed: " + std::to_string(stats_.speed_));
     screen.drawTextWithBackground(
         getCenter().x_, spacer * 4,
-        "Attack: " + std::to_string(stats_.attackPower));
+        "Attack: " + std::to_string(stats_.attackPower_));
 }
 
 Point Status::getCenter() const
