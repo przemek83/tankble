b7bb0fcf4d48f80eac9c8b185e785881b8824a41
przemek83 <4788832+przemek83@users.noreply.github.com>
2024-09-14 12:05:51 +0200

    Add tests fro Dispaly class.

---
+++ test/DisplayTest.cpp
@@ -0,0 +1,32 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <src/Display.h>
+
+TEST_CASE("Display initialization and properties", "[Display]")
+{
+    Display display;
+
+    SECTION("Initialization")
+    {
+        REQUIRE(display.getWidth() == 800);
+        REQUIRE(display.getHeight() == 600);
+    }
+
+    SECTION("Get Center")
+    {
+        REQUIRE(display.getCenterX() == 400);
+        REQUIRE(display.getCenterY() == 300);
+    }
+
+    SECTION("Set and Get Width")
+    {
+        display.setWidth(1200);
+        REQUIRE(display.getWidth() == 1200);
+    }
+
+    SECTION("Set and Get Height")
+    {
+        display.setHeight(800);
+        REQUIRE(display.getHeight() == 800);
+    }
+}
