6ad3121bee9a014a09f2b1b3bd8ebb283600c155
przemek83 <4788832+przemek83@users.noreply.github.com>
2024-09-28 18:18:21 +0200

    Add empty methods for pure virtual ones from base class.

--- test/FakeDisplay.h
+++ test/FakeDisplay.h
@@ -5,6 +5,19 @@
 class FakeDisplay : public Display
 {
 public:
+    void drawText(int x, int y, const std::string& text) const override {};
+
+    void drawTextWithBackground(int x, int y,
+                                const std::string& text) const override {};
+
+    void drawBackground(ResourceType resourceType) const override {};
+
+    void drawScaledBitmap(ResourceType resourceType, int x, int y, int width,
+                          int height) const override {};
+
+    void drawScaledBitmapWithRotation(ResourceType resourceType, int x, int y,
+                                      int size, int degrees) const override {};
+
     int getResourceWidth(
         [[maybe_unused]] ResourceType resourceType) const override
     {
@@ -19,6 +32,10 @@ public:
     void setResourceWidth(int width) { resourceWidth_ = width; }
 
     void setResourceHeight(int height) { resourceHeight_ = height; }
+
+    void useFullScreenMode() override {};
+
+    void useWindowedMode() override {};
 
 private:
     int resourceWidth_;
