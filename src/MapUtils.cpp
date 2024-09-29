38f421b055824bf520cdd7c47d12fc1b1327aac2
przemek83 <4788832+przemek83@users.noreply.github.com>
2024-09-29 08:50:20 +0200

    Some simplifications and static analysis fixes.

--- src/MapUtils.cpp
+++ src/MapUtils.cpp
@@ -8,25 +8,26 @@ std::vector<Point> getMovePoints(Point leftUpperCorner, Direction direction,
     const int tankThird{tileSize / 3};
     const int x{leftUpperCorner.x_};
     const int y{leftUpperCorner.y_};
+    const int tankTwoThird{2 * tankThird};
     switch (direction)
     {
         case Direction::UP:
         {
-            return {{x + tankThird, y}, {x + 2 * tankThird, y}};
+            return {{x + tankThird, y}, {x + tankTwoThird, y}};
         }
         case Direction::DOWN:
         {
-            return {{x + tankThird, y + tileSize - 1},
-                    {x + 2 * tankThird, y + tileSize - 1}};
+            return {{x + tankThird, (y + tileSize) - 1},
+                    {x + tankTwoThird, (y + tileSize) - 1}};
         }
         case Direction::LEFT:
         {
-            return {{x, y + tankThird}, {x, y + 2 * tankThird}};
+            return {{x, y + tankThird}, {x, y + tankTwoThird}};
         }
         case Direction::RIGHT:
         {
-            return {{x + tileSize - 1, y + tankThird},
-                    {x + tileSize - 1, y + 2 * tankThird}};
+            return {{(x + tileSize) - 1, y + tankThird},
+                    {(x + tileSize) - 1, y + tankTwoThird}};
         }
 
         default:
