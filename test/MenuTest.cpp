f2fb23e007e4d21352be501e0ab1dd7d81b148a8
przemek83 <4788832+przemek83@users.noreply.github.com>
2024-09-30 17:28:56 +0200

    Add tests for the choiceToLevel method.

--- test/MenuTest.cpp
+++ test/MenuTest.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <src/Level.h>
 #include <src/Menu.h>
 
 #include "FakeDisplay.h"
@@ -17,5 +18,15 @@ TEST_CASE("Menu usage", "[Menu]")
         REQUIRE(menu.isLevelPicked(UserChoice::LEVEL_4));
         REQUIRE(menu.isLevelPicked(UserChoice::LEVEL_MENU) == false);
         REQUIRE(menu.isLevelPicked(UserChoice::EXIT) == false);
+    }
+
+    SECTION("isLevelPicked")
+    {
+        REQUIRE(menu.choiceToLevel(UserChoice::LEVEL_1) == Level::LEVEL_1);
+        REQUIRE(menu.choiceToLevel(UserChoice::LEVEL_2) == Level::LEVEL_2);
+        REQUIRE(menu.choiceToLevel(UserChoice::LEVEL_3) == Level::LEVEL_3);
+        REQUIRE(menu.choiceToLevel(UserChoice::LEVEL_4) == Level::LEVEL_4);
+        REQUIRE(menu.choiceToLevel(UserChoice::LEVEL_MENU) == Level::LEVEL_4);
+        REQUIRE(menu.choiceToLevel(UserChoice::EXIT) == Level::LEVEL_4);
     }
 }
